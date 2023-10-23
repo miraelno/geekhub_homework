b89e59e1e52ab7bbd57e86635b3fe881d20f0254
Karina MacOS <bogdanova.karina22@gmail.com>
2023-10-23 11:36:26 +0300

    fix: task 6 is fixed

--- HT_3/task_6.py
+++ HT_3/task_6.py
@@ -1,8 +1,8 @@
 # 6. Write a script to get the maximum and minimum value in a dictionary.
 
-test_dict = {'middle1': 11, 'max': 20, 'min': 1, 'middle2': 15, 'middle3': [], 'middle4': '13'}
+test_dict = {'middle1': 11, 'max': 20.5, 'min': 1, 'middle2': 15, 'middle3': [], 'middle4': '13'}
 
-values = list(filter(lambda x: isinstance(x, int), test_dict.values()))
+values = list(filter(lambda x: isinstance(x, (int, float)), test_dict.values()))
 print(f"The max value is: {max(values)}")
 print(f"The min value is: {min(values)}")
 
