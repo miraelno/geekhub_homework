68673491f7bfb3b20e8d7519117e42a20a33f341
Mirael_No <bogdanova.karina22@gmail.com>
2023-11-04 21:00:51 +0200

    fix: fix task 7

--- HT_6/task_7.py
+++ HT_6/task_7.py
@@ -1,38 +1,24 @@
-# 7. Написати функцію, яка приймає на вхід список (через кому), підраховує кількість однакових елементів у ньомy і виводить результат. 
+# 7. Написати функцію, яка приймає на вхід список (через кому), підраховує кількість однакових елементів у ньомy і виводить результат.
 # Елементами списку можуть бути дані будь-яких типів.
 #     Наприклад:
 #     1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2] ----> "1 -> 3, foo -> 2, [1, 2] -> 2, True -> 1"
-from collections import Counter
+
+def count_same_elements(input_list: list):
+
+    while len(input_list) > 0:
+        counter = 1
+        value = input_list.pop(0)
+        for j in input_list:
+            if isinstance(value, (dict, set, list, tuple)):
+                if value == j:
+                    input_list.remove(j)
+                    counter += 1
+            else:
+                if value is j:
+                    input_list.remove(j)
+                    counter += 1
+        print(f'{value} -> {counter}')
 
 
- 
-def count_same_elements(input_list):
-    return Counter(input_list)
-#   unique_l = []
 
-#   for i in input_list:
-#     if i not in unique_l:
-#       unique_l.append(i)
-
-#   for i in unique_l:
-#     print(f'{i} -> {input_list.count(i)}')
-
-print(count_same_elements([1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]]))
-
-
-# def fun(l):
-#     handled_keys = []
-
-#     for i in l:
-#         for j in handled_keys:
-#             if j != i or type(j) != type(i):
-#                 count = 0
-#                 for j in l:
-#                     if j == i and type(j) == type(i):
-#                         count += 1
-#                         print(i, count)
-#                         handled_keys.append(i)
-#                         result = Counter(l)
-                        
-
-# print(fun([1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]]))
+print(count_same_elements([1, 1, 'foo', (1, 2), [1, 2], 'foo', 1, [1, 2], 'True', True, {1, 2}, (1, 2)]))
