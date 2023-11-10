7d42d283ad68aa8532f1b27589403805a2e921f3
Karina Bohdanova <karinabohdanova@litsadmins-MacBook-Pro.local>
2023-11-10 17:52:41 +0200

    fix: fix 3 task, adding validation

--- HT_8/task_3.py
+++ HT_8/task_3.py
@@ -11,6 +11,8 @@
 #    P.P.P.S Не забудьте обробляти невалідні ситуації (аналог range(1, -10, 5)). Подивіться як веде себе стандартний range в таких випадках.
 
 def my_range(start, stop=None, step=1):
+    if step == 0:
+        raise ValueError("Step value can't be 0")
     
     if stop is None:
         stop = start
@@ -27,14 +29,16 @@ def my_range(start, stop=None, step=1):
             start += step
 
 
-# for i in my_range(0, -10, -1):
-#     print(i)
+for i in my_range(0, 10, 0):
+    print(i)
 
-assert list(range(10)) == list(my_range(10))
-assert list(range(1, 11)) == list(my_range(1, 11))
-assert list(range(0, 30, 5)) == list(my_range(0, 30, 5))
-assert list(range(0, 10, 3)) == list(my_range(0, 10, 3))
-assert list(range(0, -10, -1)) == list(my_range(0, -10, -1))
-assert list(range(0)) == list(my_range(0))
-assert list(range(1, 0)) == list(my_range(1, 0))
-assert list(range(1, -10, 5)) == list(my_range(1, -10, 5))
+
+
+# assert list(range(10)) == list(my_range(10))
+# assert list(range(1, 11)) == list(my_range(1, 11))
+# assert list(range(0, 30, 5)) == list(my_range(0, 30, 5))
+# assert list(range(0, 10, 3)) == list(my_range(0, 10, 3))
+# assert list(range(0, -10, -1)) == list(my_range(0, -10, -1))
+# assert list(range(0)) == list(my_range(0))
+# assert list(range(1, 0)) == list(my_range(1, 0))
+# assert list(range(1, -10, 5)) == list(my_range(1, -10, 5))
