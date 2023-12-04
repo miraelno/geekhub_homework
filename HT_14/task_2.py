bb97bb2d7b59636ca0d5a3ca8c847469af8122d9
Karina Bohdanova <karinabohdanova@litsadmins-MacBook-Pro.local>
2023-12-04 16:29:36 +0200

    fix: refactoring

--- HT_14/task_2.py
+++ HT_14/task_2.py
@@ -1,4 +1,4 @@
-# 2. Створіть програму для отримання курсу валют за певний період. 
+# 2. Створіть програму для отримання курсу валют за певний період.
 # - отримати від користувача дату (це може бути як один день так і інтервал - початкова і кінцева дати, продумайте механізм реалізації) і назву валюти
 # - вивести курс по відношенню до гривні на момент вказаної дати (або за кожен день у вказаному інтервалі)
 # - не забудьте перевірку на валідність введених даних
@@ -7,27 +7,37 @@ import requests
 
 
 class CurrencyAPI:
-    
-    def __init__(self):
-        self.url_for_day = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange'
-        self.url_for_range = 'https://bank.gov.ua/NBU_Exchange/exchange_site'
+    url_for_day = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange"
+    url_for_range = "https://bank.gov.ua/NBU_Exchange/exchange_site"
 
     def get_currency_for_range(self, start, end, currency):
-        if start > end:
-            print('Start date should be before end date.')
+        if int(start) > int(end):
+            print("Start date should be before end date.")
             return
-        
-        r = requests.get(f"{self.url_for_range}?start={start}&end={end}&valcode={currency}&exchangedate&order=desc&json")
+
+        params = {
+            "start": start,
+            "end": end,
+            "valcode": currency,
+            "order": "desc",
+            "json": "",
+        }
+        r = requests.get(self.url_for_range, params=params)
         result_list = r.json()
         for record in result_list:
             print(f"{record['exchangedate']}: {round(record['rate'], 2)} ")
 
     def get_currency_for_day(self, date, currency):
-        r = requests.get(f'{self.url_for_day}?valcode={currency}&date={date}&json')
+        params = {
+            "valcode": currency,
+            "date": date,
+            "json": "",
+        }
+        r = requests.get(self.url_for_day, params=params)
         result = r.json()[0]
         print(f"{result['exchangedate']} -> {round(result['rate'], 2)}")
-        
+
 
 c = CurrencyAPI()
-c.get_currency_for_range('20220115', '20220131', 'eur')
-c.get_currency_for_day('20231203', 'eur')
+c.get_currency_for_range("20220115", "20220131", "eur")
+c.get_currency_for_day("20231203", "eur")
--- HT_14/task_3/task_3.py
+++ HT_14/task_3/task_3.py
@@ -1,4 +1,4 @@
-# 3. http://quotes.toscrape.com/ - написати скрейпер для збору всієї доступної інформації про записи: цитата, автор, інфа про автора тощо. 
+# 3. http://quotes.toscrape.com/ - написати скрейпер для збору всієї доступної інформації про записи: цитата, автор, інфа про автора тощо.
 # - збирається інформація з 10 сторінок сайту.
 # - зберігати зібрані дані у CSV файл
 
@@ -10,59 +10,44 @@ from bs4 import BeautifulSoup
 
 
 class QuotesScraper:
-    def __init__(self):
-        self.url = 'http://quotes.toscrape.com'
-        self.authors = {}
+    url = "http://quotes.toscrape.com"
+    authors = {}
 
     def add_data_to_file(self, data):
 
-        headers = ['Quote', 'Author', 'Tags', 'Born', 'Description']
-        with open('data.csv', 'a+', encoding="utf-8") as f:
-            needs_header = os.stat('data.csv').st_size == 0
+        headers = ["Quote", "Author", "Tags", "Born", "Description"]
+        with open("data.csv", "a+", encoding="utf-8") as f:
+            needs_header = os.stat("data.csv").st_size == 0
             writer_obj = csv.writer(f)
 
             if needs_header:
                 writer_obj.writerow(headers)
                 needs_header = False
-        
+
             writer_obj.writerow(data)
 
-            f.close()
-
     def get_author_info(self, url):
-        author_record = []
         r = requests.get(url)
         soup = BeautifulSoup(r.text, "html.parser")
-        born_info = soup.find('div', 'author-details').find('p').text
-        description = soup.find('div', 'author-description').text.strip()
-        
-        author_record.append(born_info)
-        author_record.append(description)
+        born_info = soup.find("div", "author-details").find("p").text
+        description = soup.find("div", "author-description").text.strip()
 
-        return author_record
+        return [born_info, description]
 
     def get_data(self):
-        new_record = []
         r = requests.get(self.url)
         while True:
             soup = BeautifulSoup(r.text, "html.parser")
-            all_quotes_on_page = soup.find_all('div', 'quote')
-            try:
-                next_page_link = soup.find('li', 'next').find('a').get('href')
-            except AttributeError:
-                print("Scrapping is finished!")
-                break
+            all_quotes_on_page = soup.find_all("div", "quote")
 
             for i in range(len(all_quotes_on_page)):
                 quote_tag = all_quotes_on_page[i]
 
-                quote_text = quote_tag.find('span', 'text').text
-                author = quote_tag.find('small', 'author').text
-                tags_list = [t.text for t in quote_tag.find_all('a', 'tag')]
-                
-                new_record.append(quote_text)
-                new_record.append(author)
-                new_record.append(tags_list)
+                quote_text = quote_tag.find("span", "text").text
+                author = quote_tag.find("small", "author").text
+                tags_list = [t.text for t in quote_tag.find_all("a", "tag")]
+
+                new_record = [quote_text, author, tags_list]
 
                 if author in self.authors.keys():
                     new_record.append(self.authors[author])
@@ -71,9 +56,14 @@ class QuotesScraper:
                     new_record.extend(self.get_author_info(link))
 
                 self.add_data_to_file(new_record)
-                new_record.clear()
 
-                r = requests.get(f"{self.url}{next_page_link}")
+            try:
+                next_page_link = soup.find("li", "next").find("a").get("href")
+            except AttributeError:
+                print("Scrapping is finished!")
+                break
+
+            r = requests.get(f"{self.url}{next_page_link}")
 
 
 q = QuotesScraper()
