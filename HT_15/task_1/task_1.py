5c132dd2c2b44b48d60175715f8b9a30cff668d0
Karina Bohdanova <karinabohdanova@litsadmins-MacBook-Pro.local>
2023-12-08 23:11:39 +0200

    feat: task_1 is finished

--- HT_15/task_1/task_1.py
+++ HT_15/task_1/task_1.py
@@ -1,12 +1,16 @@
-# 1. Викорисовуючи requests, написати скрипт, який буде приймати на вхід ID категорії із сайту https://www.sears.com і буде збирати всі товари із цієї категорії, 
+# 1. Викорисовуючи requests, написати скрипт, який буде приймати на вхід ID категорії із сайту https://www.sears.com і буде збирати всі товари із цієї категорії,
 # збирати по ним всі можливі дані (бренд, категорія, модель, ціна, рейтинг тощо) і зберігати їх у CSV файл (наприклад, якщо категорія має ID 12345,
 # то файл буде називатись 12345_products.csv)
 # Наприклад, категорія https://www.sears.com/tools-tool-storage/b-1025184 має ІД 1025184
 
-import json
+import csv
+import os
 
 import requests
 from fake_useragent import UserAgent
+
+
+category_id = input("Enter category id: ").strip()
 
 
 def get_data(category_id):
@@ -15,27 +19,44 @@ def get_data(category_id):
         "searchType": "category",
         "store": "Sears",
         "storeId": 10153,
-        "catGroupId": 5000928,
+        "catGroupId": category_id,
     }
     r = requests.get(
-        url='https://www.sears.com/api/sal/v3/products/search',
-        headers={"Authorization": "SEARS",
-                "User-Agent": ua.random},
+        url="https://www.sears.com/api/sal/v3/products/search",
+        headers={"Authorization": "SEARS", "User-Agent": ua.random},
         params=params,
     )
 
-    with open('test.json', 'w') as f:
-        f.write(json.dumps(r.json(), indent=4))
-    
     return r.json()
 
 
-def get_mocked_data():
-    with open('test.json', 'r') as f:
-        data = json.loads(f.read())
-    
-    return data
+def write_to_csv(category_id, data):
+    headers = ["BrandName", "Name", "Category", "FinalPrice"]
+
+    with open(f"{category_id}_products.csv", "a+", encoding="utf-8") as f:
+        needs_header = os.stat(f"{category_id}_products.csv").st_size == 0
+        writer_obj = csv.writer(f, delimiter=",")
+
+        if needs_header:
+            writer_obj.writerow(headers)
+
+        writer_obj.writerow(data)
 
 
-# get_data()
-print(get_mocked_data())
+def find_product_info():
+    data = get_data(category_id)
+    product_items = data["items"]
+
+    for item in product_items:
+        data = [
+            item["brandName"],
+            item["name"],
+            item["category"],
+            item["price"]["messageTags"]["finalPrice"],
+        ]
+        write_to_csv(category_id, data)
+
+    print("Finished!")
+
+
+find_product_info()
