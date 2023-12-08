2ca8e124b1f4f51d2171a2e99855b537e6e2df04
Karina Bohdanova <karinabohdanova@litsadmins-MacBook-Pro.local>
2023-12-09 00:56:48 +0200

    feat: task_2 is finished

--- HT_15/task_2/task_2.py
+++ HT_15/task_2/task_2.py
@@ -1,3 +1,67 @@
 # 2. Викорисовуючи requests, заходите на ось цей сайт "https://www.expireddomains.net/deleted-domains/" (з ним будьте обережні),
-#  вибираєте будь-яку на ваш вибір доменну зону і парсите список  доменів - їх там буде десятки тисяч 
+#  вибираєте будь-яку на ваш вибір доменну зону і парсите список  доменів - їх там буде десятки тисяч
 # (звичайно ураховуючи пагінацію). Всі отримані значення зберігти в CSV файл.
+
+import csv
+import os
+import time
+
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+
+
+def get_data(params=None):
+    ua = UserAgent()
+
+    r = requests.get(
+        url="https://www.expireddomains.net/expired-domains/",
+        params=params,
+        headers={"User-Agent": ua.random},
+    )
+
+    return r.content
+
+
+def write_to_csv(data):
+    headers = ["Domain"]
+
+    with open("domains.csv", "a+", encoding="utf-8") as f:
+        needs_header = os.stat("domains.csv").st_size == 0
+        writer_obj = csv.writer(f)
+
+        if needs_header:
+            writer_obj.writerow(headers)
+
+        for row in data:
+            writer_obj.writerow([row])
+
+
+def find_info():
+    site_content = get_data()
+    counter_page = 25
+
+    while True:
+        soup = BeautifulSoup(site_content, features="html.parser")
+
+        rows = soup.find_all("td", "field_domain")
+        domain_text_list = []
+
+        for row in rows:
+            domain_text_list.append(row.text)
+
+        write_to_csv(domain_text_list)
+
+        try:
+            soup.find("a", "next")
+        except AttributeError:
+            print("Scrapping is finished!")
+            return
+
+        params = {"start": counter_page}
+        counter_page += 25
+        time.sleep(10)
+        site_content = get_data(params)
+
+
+find_info()
